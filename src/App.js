4507ccd26ff65fe25b47f438bae4834e698baddf
radovanne3 <radovanne3@gmail.com>
2021-07-08 03:28:01 +0200

    Axios.get integration and testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import { Component } from "react";
 import './App.css';
+import axios from "axios";
 
 
 
 export default class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    const users = axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => this.setState({users : res.data}))
+  }
+
   render(){
-    return (<h1>Basic React App</h1>)
+    return (
+        <div>
+          {this.state.users.map(user =>
+              <div key={user.id}>
+                <p>name: {user.name}</p>
+                <p>email: {user.email}</p>
+                <hr/>
+              </div>)}
+        </div>)
   }
 }
