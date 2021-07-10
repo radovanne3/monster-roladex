474c16edbdaa7db31cf951a701500b05f3e4ad23
snorlax <radovanne3@gmail.com>
2021-07-10 22:54:53 +0200

    Create SearchBox component and added styling, integrated searching against monsters name and email (include), it filters as we type, Minor code cleaning and finishing up

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { Component } from "react";
 import './App.css';
 import API_URL from "./constants";
 import {CardList} from "./components/card-list/card-list.component";
+import {SearchBox} from "./components/search-box/search-box.component";
 
 
 export default class App extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      monsters: []
-    }
+        monsters: [],
+        searchField: ``
+    };
   }
   componentDidMount() {
     API_URL.get(`/users`)
@@ -33,9 +35,22 @@ export default class App extends Component{
   });
   }
 
+  handleChange = (e) => {
+      this.setState({searchField: e.target.value})
+  }
+
   render(){
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()
+    ) || monster.email.toLowerCase().includes(searchField.toLowerCase()))
     return (
-            <CardList monsters ={this.state.monsters}></CardList>
+           <div className="App" >
+               <h1>Monsters Rolodex</h1>
+               <SearchBox placeholder='search monsters'
+                          handleChange={this.handleChange}
+               />
+               <CardList monsters ={filteredMonsters}/>
+           </div>
         )
   }
 }
